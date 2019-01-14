ffba071107497fa7ea1f490193cddb68d6d06c96
nagipan777 <nagipan777@gmail.com>
2019-01-14 21:02:10 +0900

    paser of hander?

--- main.py
+++ main.py
@@ -62,13 +62,6 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
